bbc6151ea7fff1482d95e97382f3dc71c4c76a80
Daniil Veremchuk <daniel@verydanny.com>
2018-09-05 22:13:22 -0400

    only extractCss if not inserted

--- src/extractCss.js
+++ src/extractCss.js
@@ -2,28 +2,37 @@
 import { contextKey, contextSecret } from './const'
 
 const extractCss = ( GlobalContext ) => () => {
-  const { styles, globalCss } = GlobalContext[ contextSecret ]
+  const { styles, globalCss, inserted } = GlobalContext[ contextSecret ]
   let target = {
     css: '',
     mediaQueries: ''
   }
   
   Object.keys(styles).reduce((acc, curr) => {
-    const currentCss = styles[curr].body && styles[curr].body.css
+
+    const match = new RegExp(`${contextKey}-([a-zA-Z0-9-+/]+)`, "g")
+    const ids = curr.split(match)
+    const id = ids[1]
+
+    if ( inserted[id] ) {
+      return acc
+    } else {
+      const currentCss = styles[curr].body && styles[curr].body.css
       ? styles[curr].body.css
       : false
-    const currentMediaQuery = styles[curr].body && styles[curr].body.mediaQuery
-      ? styles[curr].body.mediaQuery
-      : false
+      const currentMediaQuery = styles[curr].body && styles[curr].body.mediaQuery
+        ? styles[curr].body.mediaQuery
+        : false
 
-    if (currentCss) {
-      acc.css += currentCss
+      if (currentCss) {
+        acc.css += currentCss
 
-      if (currentMediaQuery) {
+        if (currentMediaQuery) {
+          acc.mediaQueries += currentMediaQuery
+        }
+      } else if (currentMediaQuery) {
         acc.mediaQueries += currentMediaQuery
       }
-    } else if (currentMediaQuery) {
-      acc.mediaQueries += currentMediaQuery
     }
 
     return acc
