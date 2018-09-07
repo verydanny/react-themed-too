ce910acc46f545a38a080bba7e3dc11b4966694a
Daniil Veremchuk <daniel@verydanny.com>
2018-09-06 20:57:42 -0400

    passing 2 suites

--- src/__tests__/compose.test.js
+++ src/__tests__/compose.test.js
@@ -3,18 +3,31 @@ import nodeEval from "node-eval"
 
 import compiler from '../../compiler'
 import paths from '../../paths'
+import { locals } from '../mocks/test.css'
 
-test('Testing basic CSS conversion', async () => {
-  const stats = await compiler(path.resolve(paths.TEST, 'css/test.css'))
-  const cssFileName = new RegExp('test.css')
+let cssFile
 
-  const cssFileModule = stats.toJson().modules.filter(( module ) => {
-    if ( module.id.match(cssFileName) ) {
+// Convert this to read multiple css files and scss files
+
+beforeAll(async () => {
+  const stats = await compiler(path.resolve(paths.TEST, "css/test.css"))
+  const cssFileName = new RegExp("test.css")
+
+  const cssFileModule = stats.toJson().modules.filter(module => {
+    if (module.id.match(cssFileName)) {
       return true
     } else {
       return false
     }
   })[0]
-  
-  const cssFile = nodeEval(cssFileModule.source, 'css/test.js')
+
+  cssFile = nodeEval(cssFileModule.source, "css/test.js")
+}, 6000)
+
+test('Webpack basic success', () => {
+  expect(cssFile.locals).toBeTruthy()
+})
+
+test('Locals matches mock', () => {
+  expect(cssFile.locals).toEqual(locals)
 })
